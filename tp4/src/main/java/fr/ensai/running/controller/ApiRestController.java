679c3bcd1fbda9177aa33c7e116b3e02bb5e9549
edln6 <eulalie.delaune@eleve.ensai.fr>
2025-04-07 08:57:05 +0000

    endpoints

--- tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
+++ tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
@@ -3,7 +3,11 @@ package fr.ensai.running.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +28,31 @@ public class ApiRestController {
     public List<Athlete> allAthletes() {
 
         return athleteService.findAll();
+    }
+
+    /**
+     * Get an athlete by id
+     */
+    @GetMapping("/athlete/{id}")
+    public Athlete findAthletesById(@PathVariable("id") Long id) {
+
+        return athleteService.findById(id);
+    }
+
+    /**
+     * Delete an athlete by id
+     */
+    @DeleteMapping("/delete/{id}")
+    public void deleteAthlete(@PathVariable("id") Long id) {
+        athleteService.deleteById(id);
+    }
+
+    /**
+     * Create an athlete
+     */
+    @PostMapping("/create")
+    public Athlete createAthlete(@RequestBody Athlete athlete) {
+        return athleteService.save(athlete);
     }
 
 }
